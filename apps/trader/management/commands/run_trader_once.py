398581ed8f38468e6273616f628439a3e076fa16
배종수 <ocpek@kakao.com>
2026-03-12 19:08:21 +0900

    트레이딩 불필요한 LLM 호출 없애기

--- apps/trader/management/commands/run_trader_once.py
+++ apps/trader/management/commands/run_trader_once.py
@@ -1,14 +1,26 @@
 from __future__ import annotations
 
+from datetime import time
+from zoneinfo import ZoneInfo
+
 from django.core.management.base import BaseCommand
+from django.utils import timezone
 
 from apps.trader.services import run_trading_cycle
+
+KST = ZoneInfo("Asia/Seoul")
+MARKET_OPEN = time(9, 0)
+MARKET_CLOSE = time(15, 30)
 
 
 class Command(BaseCommand):
     help = "트레이딩 사이클을 1회 실행합니다."
 
     def handle(self, *args, **options):
+        now_kst = timezone.now().astimezone(KST)
+        if not (MARKET_OPEN <= now_kst.time() <= MARKET_CLOSE):
+            return
+
         decision = run_trading_cycle()
 
         self.stdout.write("트레이딩 1회 실행 완료")
